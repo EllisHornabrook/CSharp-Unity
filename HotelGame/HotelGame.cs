99a09446b4acd1f6e14e5f5e16475ca8ea67a35a
Ellis Hornabrook <ellishornabrook@gmail.com>
2021-02-09 17:11:27 +0000

    fixed error on button mash

--- HotelGame/HotelGame.cs
+++ HotelGame/HotelGame.cs
@@ -27,13 +27,13 @@ public class HotelGame : MonoBehaviour
     private void ManageState()
     {
         var nextStates = state.GetNextStates();
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+
+        for (int i = 0; i < nextStates.Length; i++)
         {
-            state = nextStates[0];
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            state = nextStates[1];
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                state = nextStates[i];
+            }
         }
         textComponent.text = state.GetStoryState();
     }
