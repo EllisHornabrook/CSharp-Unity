d7b7140dd8da4bfc27b2ec56dc9a008f5451a8c0
Ellis Hornabrook <ellishornabrook@gmail.com>
2021-02-11 19:44:29 +0000

    move game paddle with mouse

---
+++ BlockBreaker/Paddle.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float screenWidthInUnits = 16f;
+
+    void Update()
+    {
+        float mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
+        paddlePosition.x = Mathf.Clamp(mousePositionInUnits, minX, maxX);
+        transform.position = paddlePosition;
+    }
+}
