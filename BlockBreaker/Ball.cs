761454db80aeedb324c21c1f235337edcdaa6719
Ellis Hornabrook <ellishornabrook@gmail.com>
2021-02-15 18:38:53 +0000

    player start/shoot ball functionality

---
+++ BlockBreaker/Ball.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 0f;
+    [SerializeField] float yPush = 15f;
+
+    Vector2 paddleToBallVector;
+    bool gameStart = false;
+
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+    }
+
+    
+    void Update()
+    {
+        if (!gameStart)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            gameStart = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePosition + paddleToBallVector;
+    }
+}
